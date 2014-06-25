f4c9c2b771d91b10f85390eac3387dde737f23e8
Denis Pismenny <denis.pismenny@gmail.com>
2014-06-26 00:42:02 +0300

    sidebar done

--- js/common.js
+++ js/common.js
@@ -1,14 +1,15 @@
-$(window).load(function() {
+head.ready(function() {
 
 	$(function(){
 		var bg_image = (Math.floor(Math.random() * 11))+ ".jpg";
 		$('.bg_cover').css({'background-image': 'url(img/covers/bg' + bg_image + ')'});
 	});
 
-});
-
-head.ready(function() {
-
+	$(".js-select select").on("change",function(){
+		var	val = $(this).val();
+		$(this).parent().find(".js-select-input").val(val);
+		$(this).parent().find(".js-select-text").text(val);
+	});
 
 	$(function() {
 		var sidebar_height = $('.l-sidebar').height();
